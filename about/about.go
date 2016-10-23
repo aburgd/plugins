cb2b86f9ae85e9ea7eae9a32620b6738a4a50686
Alec Burgdorf <aeburgd@gmail.com>
2016-10-22 20:20:22 -0500

    check if user is op or owner

--- about/about.go
+++ about/about.go
@@ -12,8 +12,11 @@ func about(command *bot.Cmd) (msg string, err error) {
     if err != nil {
       return "shit happened", err
     }
-
-    msg = fmt.Sprintf("if I'm broken, please contact my owner with the subject line %s at %s", subjectLine, email)
+    if command.User.Nick == "ghostpage" || command.User.Nick == "TyGently" {
+      msg = fmt.Sprintf("if I'm broken, please contact my owner with the subject line %s at %s", subjectLine, email)
+    } else {
+      msg = fmt.Sprint("sorry, you're not allowed to do that!")
+    }
     return
 }
 
