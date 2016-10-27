a4e5ad76732df05282ea248e94a2fee0936bd317
Alec Burgdorf <aeburgd@gmail.com>
2016-10-27 18:53:13 -0500

    add support function

---
+++ support/support.go
@@ -0,0 +1,23 @@
+package support
+
+import (
+  "fmt"
+
+  "github.com/go-chat-bot/bot"
+)
+
+var kofiLink = "http://ko-fi.com/A642DU2"
+
+func support(command *bot.Cmd) (msg string) {
+  msg = fmt.Sprintf("Please support my creator, the almighty eng13, by buying them a coffee: %s", kofiLink)
+
+  return
+}
+
+func init() {
+  bot.RegisterCommand(
+    "support",
+    "tell user how to support creator",
+    "",
+    support)
+}
